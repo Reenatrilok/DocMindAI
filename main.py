4230f5190ff30c839d216c54fc82d09903129530
Reena Trilok <reenatrilok36@gmail.com>
2026-05-31 01:14:42 +0530

    Added PDF upload UI and text extraction page

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pypdf import PdfReader
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Welcome to DocMind AI"}
+templates = Jinja2Templates(directory="templates")
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
+
+
+@app.post("/upload", response_class=HTMLResponse)
+async def upload_pdf(request: Request,
+                     file: UploadFile = File(...)):
+
+    file_path = os.path.join(
+        UPLOAD_FOLDER,
+        file.filename
+    )
+
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+
+    reader = PdfReader(file_path)
+
+    text = ""
+
+    for page in reader.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text + "\n"
+
+    return templates.TemplateResponse(
+        request=request,
+        name="result.html",
+        context={
+            "filename": file.filename,
+            "text": text[:5000]
+        }
+    )
